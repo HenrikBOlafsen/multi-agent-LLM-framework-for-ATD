0ff463fd1b072ea42022bb32c86838b1743f840a
HenrikBOlafsen <olafsen.henrik@gmail.com>
2025-10-28 19:01:09 +0100

    removed outdated test project

--- projects_to_analyze/customTestProject/src/customTestProject/textUpgrader.py
+++
@@ -1,7 +0,0 @@
-from textPadder import padTextTopBottom
-
-def addDashesToText(text: str):
-    return "--- " + text
-
-def addDashesAboveAndBelowText(text: str):
-    return padTextTopBottom("------------------------------------------\n" + text + "\n------------------------------------------")
